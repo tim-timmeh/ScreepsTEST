123fd70a397c37c70a2f9fc3f7918a6d1c75cdb9
tim_timmeh <tim_timmeh@hotmail.com>
2018-06-20 08:31:20 +0800

    Tower repair lowest structure HP

--- role.tower.js
+++ role.tower.js
@@ -1,17 +1,21 @@
+//++ Target creeps without tough first?
+
 var roleTower = {
 
 	run: function(tower) {
 		var enemy = tower.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
 		var targetsMyRepair = tower.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter : (hp) => hp.hits < hp.hitsMax})
- 		var targetsRepair = tower.pos.findClosestByPath(FIND_STRUCTURES, {filter : (hp) => hp.hits < hp.hitsMax})
-
+ 		//--var targetsRepair = tower.pos.findClosestByPath(FIND_STRUCTURES, {filter : (hp) => hp.hits < hp.hitsMax})
+		// get all damaged structures and list from lowest hits
+		var targetsRepairLowestHp = tower.find(FIND_STRUCTURES, {filter : (hp) => hp.hits < hp.hitMax})
+		targetsRepairLowestHp.sort((a,b) => a.hits - b.hits)
 		if (enemy) {
-			console.log("Enemy Found, 🔫 Attacking " + enemy);
+			console.log("Enemy Found, \ud83d\udd2b Attacking " + enemy);
 			tower.attack(enemy);
 		} else if (targetsMyRepair) {
 			tower.repair(targetsMyRepair)
-		} else if (targetsRepair && tower.energy > (tower.energyCapacity * 0.75)) {
-			tower.repair(targetsRepair)
+		} else if (targetsRepairLowestHp && tower.energy > (tower.energyCapacity * 0.75)) {
+			tower.repair(targetsRepairLowestHp[0])
 		}
 	}
 };
