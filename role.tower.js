1f023d7ea6cd1159b65e7392800b0ffa1e9ac7c3
tim_timmeh <tim_timmeh@hotmail.com>
2018-06-20 08:47:35 +0800

    typo

--- role.tower.js
+++ role.tower.js
@@ -7,7 +7,7 @@ var roleTower = {
 		var targetsMyRepair = tower.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter : (hp) => hp.hits < hp.hitsMax})
  		//--var targetsRepair = tower.pos.findClosestByPath(FIND_STRUCTURES, {filter : (hp) => hp.hits < hp.hitsMax})
 		// get all damaged structures and list from lowest hits
-		var targetsRepairLowestHp = tower.find(FIND_STRUCTURES, {filter : (hp) => hp.hits < hp.hitMax})
+		var targetsRepairLowestHp = tower.room.find(FIND_STRUCTURES, {filter : (hp) => hp.hits < hp.hitsMax})
 		targetsRepairLowestHp.sort((a,b) => a.hits - b.hits)
 		if (enemy) {
 			console.log("Enemy Found, \ud83d\udd2b Attacking " + enemy);
