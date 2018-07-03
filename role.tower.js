63acf3d0ec8cfc325f648500bffccb70ebf826c1
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2018-07-03 15:45:11 +0800

    _Sort my buildings 50% E, then All buildings 75% E

--- role.tower.js
+++ role.tower.js
@@ -8,24 +8,21 @@ var roleTower = {
 		var targetsMyRepair = tower.room.find(FIND_MY_STRUCTURES, {filter : (hp) => hp.hits < (hp.hitsMax - 800)});
  		//--var targetsRepair = tower.pos.findClosestByPath(FIND_STRUCTURES, {filter : (hp) => hp.hits < hp.hitsMax})
 		// get all damaged structures and list from lowest hits
-		var targetsRepairLowestHp = tower.room.find(FIND_STRUCTURES, {filter : (hp) => hp.hits < (hp.hitsMax - 800) && hp.hits < 30000000});
-		var targetsContainerRepair = tower.room.find(FIND_STRUCTURES, {filter : (hp) => hp.hits < (hp.hitsMax - 800) && hp.hits < 30000000 && hp.structureType == STRUCTURE_CONTAINER});
+		var targetsRepair = tower.room.find(FIND_STRUCTURES, {filter : (hp) => hp.hits < (hp.hitsMax - 800) && hp.hits < 30000000});
 		var targetsCreepRepair = _.filter(Game.creeps, (creep) => creep.hits < creep.hitsMax);
-		targetsMyRepair.push(...targetsContainerRepair)
+		targetsRepair.sort((a,b) => a.hits - b.hits);
 		targetsMyRepair.sort((a,b) => a.hits - b.hits);
-		targetsRepairLowestHp.sort((a,b) => a.hits - b.hits);
 		if (enemy) {
 			console.log("Enemy Found, \ud83d\udd2b Attacking " + enemy);
 			tower.attack(enemy);
 		// Heal creeps while above 25% energy
-	} else if (targetsCreepRepair != "" && tower.energy > (tower.energyCapacity * 0.25)) {
+	    } else if (targetsCreepRepair != "" && tower.energy > (tower.energyCapacity * 0.25)) {
 			tower.heal(targetsCreepRepair[0]);
-		// Repair my structures + container while above 50% energy
-	} else if (targetsMyRepair && tower.energy > (tower.energyCapacity * 0.5)) {
+		// Repair my structures while above 50% energy
+	    } else if (targetsMyRepair && targetsMyRepair[0].hits < 100000 && tower.energy > (tower.energyCapacity * 0.5)) {
 			tower.repair(targetsMyRepair[0]);
-		// Repair all structures while above 75% energy
-		} else if (targetsRepairLowestHp && tower.energy > (tower.energyCapacity * 0.75)) {
-			tower.repair(targetsRepairLowestHp[0]);
+		} else if (targetsRepair && tower.energy > (tower.energyCapacity * 0.75)) {
+		    tower.repair(targetsMyRepair[0]);
 		}
 	}
 };
