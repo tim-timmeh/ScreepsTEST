11d31f6d49b0ad100e7e5418988555809ae30090
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2018-07-06 15:59:14 +0800

    Creates container & Memory but doesnt delete?

--- role.miner.js
+++ role.miner.js
@@ -5,13 +5,25 @@
 var roleMiner = {
 
   run: function (creep) {
-    let source = Game.getObjectById(creep.memory.minerSource)
-		if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+    var source = Game.getObjectById(creep.memory.minerSource)
+    var structures = [];
+	if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
       if (creep.moveByPath(creep.memory.pathing) < 0 || !source.pos.isEqualTo(creep.memory.pathing[creep.memory.pathing.length - 1])){
         creep.memory.pathing = creep.pos.findPathTo(source,{ignoreCreeps : true});
       }
-      //creep.moveTo(source, {visualizePathStyle: {stroke: '#fa0'}});
-	  }
-  }
+    //creep.moveTo(source, {visualizePathStyle: {stroke: '#fa0'}});
+    } else if ((structures = creep.pos.lookFor(LOOK_STRUCTURES)) != undefined && structures.length < 1) {
+      var construction = creep.pos.lookFor(LOOK_CONSTRUCTION_SITES)
+      if (construction.length < 1) {
+        creep.pos.createConstructionSite(STRUCTURE_CONTAINER);
+      } else if (_.sum(creep.carry) == creep.carryCapacity) {
+        creep.build(construction[0])
+      }
+    } else if (Memory.containers == undefined){
+        Memory.containers = [];
+    } else if (structures != undefined && Memory.containers.indexOf(structures[0].id) == -1) {
+        Memory.containers.push(structures[0].id)
+    }
+    }
 }
 module.exports = roleMiner;
