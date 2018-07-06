0e3b715046923a2da7e5dbfc9b214e6aff23816f
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2018-07-06 13:29:40 +0800

    MemoryPathing
    Accadently overwrote miner with repairer, this is to revert

--- role.miner.js
+++ role.miner.js
@@ -1,69 +1,17 @@
 "use strict"
-var roleBuilder = require("role.builder");
-var roleRepairer = {
+/*
+ for range miners - container underneath? mine : construction underneath ? energy at 50 ? continue construction : mine : build container;
+*/
+var roleMiner = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-
-	    if (creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
-            creep.say("\u26CF harvest");
-	    }
-	    if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.building = true;
-	        creep.say("\ud83d\udd27 Repair");
-	    }
-
-	    if(creep.memory.building) {
-	        var targetsRepair = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter : (hp) => hp.hits < (hp.hitsMax - 500) && hp.hits < 90000000});
-          var targetsT = creep.room.find(FIND_STRUCTURES, {
-              filter: (structure) => {
-                  return (structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-              }
-          })
-	        //console.log("Repairing: " + targetsRepair + " - " + targetsRepair.hits + "/" + targetsRepair.hitsMax)
-            if (targetsT.length > 0) {
-                targetsT.sort((a,b) => a.energy - b.energy)
-                if(creep.transfer(targetsT[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                  if (creep.moveByPath(creep.memory.pathing) < 0 || !targetsT[0].pos.isEqualTo(creep.memory.pathing[creep.memory.pathing.length - 1])){
-                    creep.memory.pathing = creep.pos.findPathTo(targetsT[0],{ignoreCreeps : true});
-                  }
-                    //creep.moveTo(targetsT[0], {visualizePathStyle: {stroke: "#fff"}});
-                };
-            } else if (targetsRepair) {
-                if (creep.repair(targetsRepair) == ERR_NOT_IN_RANGE) {
-                  if (creep.moveByPath(creep.memory.pathing) < 0 || !targetsRepair.pos.isEqualTo(creep.memory.pathing[creep.memory.pathing.length - 1])){
-                    creep.memory.pathing = creep.pos.findPathTo(targetsRepair,{ignoreCreeps : true});
-                  }
-                    //creep.moveTo(targetsRepair, {visualizePathStyle: {stroke: "#fff"}});
-                };
-            } else {
-              roleBuilder.run(creep);        // Next role
-            }
-	    }
-	    else {
-	        // **Change to find closest energy
-	        var sources = creep.pos.findClosestByPath(FIND_SOURCES);
-          var targetsS = creep.room.find(FIND_MY_STRUCTURES, {
-              filter: (s) => {
-                  return (s.structureType == STRUCTURE_STORAGE);
-              }
-          });
-          if (targetsS != "" && creep.room.storage.store[RESOURCE_ENERGY] > 0){
-            if(creep.withdraw(targetsS[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-              if (creep.moveByPath(creep.memory.pathing) < 0 || !targetsS[0].pos.isEqualTo(creep.memory.pathing[creep.memory.pathing.length - 1])){
-                creep.memory.pathing = creep.pos.findPathTo(targetsS[0],{ignoreCreeps : true});
-              }
-            //creep.moveTo(targetsS[0], {visualizePathStyle: {stroke: "#fff"}});
-            }  // **Change to find closest energy
-          } else if(creep.harvest(sources) == ERR_NOT_IN_RANGE) {
-            if (creep.moveByPath(creep.memory.pathing) < 0 || !sources.pos.isEqualTo(creep.memory.pathing[creep.memory.pathing.length - 1])){
-              creep.memory.pathing = creep.pos.findPathTo(sources,{ignoreCreeps : true});
-            }
-                //creep.moveTo(sources, {visualizePathStyle: {stroke: '#fa0'}});
-            }
-	    }
-	}
-};
-
-module.exports = roleRepairer;
+  run: function (creep) {
+    let source = Game.getObjectById(creep.memory.minerSource)
+		if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+      if (creep.moveByPath(creep.memory.pathing) < 0 || !source.pos.isEqualTo(creep.memory.pathing[creep.memory.pathing.length - 1])){
+        creep.memory.pathing = creep.pos.findPathTo(source,{ignoreCreeps : true});
+      }
+      //creep.moveTo(source, {visualizePathStyle: {stroke: '#fa0'}});
+	  }
+  }
+}
+module.exports = roleMiner;
