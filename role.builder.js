7799ca39e9b15a09c52bfca6cb325225d5a39d95
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2018-07-07 16:38:29 +0800

    added check for targetsS != ""

--- role.builder.js
+++ role.builder.js
@@ -32,7 +32,7 @@ var roleBuilder = {
               return (s.structureType == STRUCTURE_STORAGE);
           }
       });
-      if (targetsS && creep.room.storage.store[RESOURCE_ENERGY] > 0){
+      if (targetsS != "" && creep.room.storage.store[RESOURCE_ENERGY] > 0) {
         if(creep.withdraw(targetsS[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(targetsS[0], {visualizePathStyle: {stroke: "#fff"}});
         }  // **Change to find closest energy
