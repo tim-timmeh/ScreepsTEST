1a87575cf244fcd39ff9432f42c1fb93c39d278a
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2018-07-07 16:45:26 +0800

    temp miner fix from pathfinding

--- role.miner.js
+++ role.miner.js
@@ -1,14 +1,25 @@
-"use strict"
-/*
- for range miners - container underneath? mine : construction underneath ? energy at 50 ? continue construction : mine : build container;
-*/
+"use strict";
+require("moveToModule")
+var roomMinerals
 var roleMiner = {
-
-  run: function (creep) {
-    let source = Game.getObjectById(creep.memory.minerSource)
-		if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(source, {visualizePathStyle: {stroke: '#fa0'}});
-	  }
+  run: function(creep) {
+    var source = Game.getObjectById(creep.memory.minerSource);
+    var structures = [];
+    //if (creep.harvest(source) == ERR_NOT_ENOUGH_RESOURCES && (roomMinerals = spawn.room.find(FIND_MINERALS)) == source)
+    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+      creep.moveToModule(source);
+    } else if (creep.harvest(source) == !ERR_BUSY && ((structures = creep.pos.lookFor(LOOK_STRUCTURES)) != undefined && structures.length < 1)) {
+      var construction = creep.pos.lookFor(LOOK_CONSTRUCTION_SITES);
+      if (construction.length < 1) {
+        creep.pos.createConstructionSite(STRUCTURE_CONTAINER);
+      } else if (_.sum(creep.carry) >= (creep.carryCapacity - 25)) {
+        creep.build(construction[0]);
+      }
+    } else if (Memory.containers == undefined) {
+      Memory.containers = [];
+    } else if ((structures != undefined && structures != "") && Memory.containers.indexOf(structures[0].id) == -1) {
+      Memory.containers.push(structures[0].id);
+    }
   }
-}
+};
 module.exports = roleMiner;
