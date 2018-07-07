a523d6eb9484e7b2741781e9276f7ee64671dd6d
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2018-07-07 12:48:01 +0800

    working pathing prototype

---
+++ moveToModule.js
@@ -0,0 +1,25 @@
+"use strict";
+var positionMem;
+
+Creep.prototype.moveToModule = function(destination) {
+  if (this.moveByPath(this.memory.pathing) < 0 || !destination.pos.isEqualTo(this.memory.pathing[this.memory.pathing.length - 1].x, this.memory.pathing[this.memory.pathing.length - 1].y)) {
+    this.memory.pathing = this.pos.findPathTo(destination, {
+      ignoreCreeps: true
+    });
+  } else if (Game.time % 2 == 0) {
+    if (this.memory.position != undefined) {
+      positionMem = new RoomPosition(this.memory.position.x, this.memory.position.y, this.memory.position.roomName);
+    }
+    if (this.memory.position != undefined && this.pos.toString() == positionMem.toString()) {
+      console.log("STUCK DETECTED, FINDING NEW PATH");
+      this.memory.pathing = this.pos.findPathTo(destination, {
+        ignoreCreeps: false
+      });
+    } else {
+      this.memory.position = this.pos;
+
+    }
+  }
+};
+
+module.exports = Creep.prototype.moveToModule;
