6f2dc6de8a8538d2680eaa57bbb38da1435eb835
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2018-07-09 14:26:18 +0800

    moved heal to if no enemy

--- role.attacker.js
+++ role.attacker.js
@@ -9,17 +9,16 @@ var roleAttacker = {
   /** @param {Creep} creep **/
   run: function(creep) {
     attackerFlag = _.filter(Game.flags, f => f.name == "attackerFlag")
-    if (creep.pos.roomName != attackerFlag[0].pos.roomName) {
+    if (attackerFlag[0] && creep.pos.roomName != attackerFlag[0].pos.roomName) {
      creep.moveToModule(attackerFlag[0].pos)
     } else {
-      if (creep.hits < creep.hitsMax) {
-        creep.heal(creep)
-      }
       var enemy = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS)
-      if (enemy.length) {
+      if (enemy) {
         if (creep.attack(enemy) == ERR_NOT_IN_RANGE) {
           creep.moveToModule(enemy);
         }
+      } else if (creep.hits < creep.hitsMax) {
+        creep.heal(creep)
       }
     }
   }
