df42a087770427cd42741e7f92c2e027994640fe
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2018-07-09 08:14:32 +0800

    if no flag suicide

--- role.claimer.js
+++ role.claimer.js
@@ -6,6 +6,9 @@ var roleClaimer = {
   /** @param {Creep} creep **/
   run: function(creep) {
     claimFlag = _.filter(Game.flags, f => f.name == "claimFlag")
+    if (!claimFlag) {
+        creep.suicide
+    }
     if (creep.pos.roomName != claimFlag[0].pos.roomName) {
      creep.moveToModule(claimFlag[0].pos);
     } else if (creep.room.controller.reservation != undefined) {
